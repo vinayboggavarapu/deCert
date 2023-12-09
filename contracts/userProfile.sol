a64e5db9a861c47d239deee2718c9d3c7def3e74
Mujahid002 <mujahidshaik2002@gmail.com>
2023-12-09 19:09:53 +0530

    added main functions

--- contracts/userProfile.sol
+++ contracts/userProfile.sol
@@ -1,21 +1,75 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
 
-pragma solidity ^0.8.23;
+error DeCert__OnlyIssuerRequired();
+error DeCert__OnlyUser();
+error DeCert__Invalid__User();
+error DeCert__AlreadyExists();
 
-contract userProfile{
-    struct detail {
-        string _userName;
-        string _email;
-    }
-    mapping(address=>detail) private _details;
-
-    function addDetails(address userAddress, string memory userName, string memory email) external{
-        detail memory _detail=detail({
-            _userName=userName;
-            _email=email;
-        })
-
-        _details[userAddress]=_detail;
+contract DeCert {
+    struct Detail {
+        string userName;
+        string email;
+        bool prover;
+        bool isIssuer;
     }
 
+    mapping(address => Detail) public s_details;
+    mapping(string => string[]) public s_userData;
+
+    event UserSignIn(address indexed _userAddress, string _userName);
+
+    function addDetails(
+        address _userAddress,
+        string memory _userName,
+        string memory _email,
+        bool _prover,
+        bool _ofIssuer
+    ) external IsNotExisting(_userAddress) {
+        Detail memory userDetail = Detail({
+            userName: _userName,
+            email: _email,
+            prover: _prover,
+            isIssuer: _ofIssuer
+        });
+
+        s_details[_userAddress] = userDetail;
+        emit UserSignIn(_userAddress, _userName);
+    }
+
+    // Fetch Details of any particular address
+    function fetchDetail(address userAddress) external view returns (Detail memory) {
+        if (!checkDetail(userAddress)) revert DeCert__Invalid__User();
+        return s_details[userAddress];
+    }
+
+    // Fetch User's Data/Certifications
+    function fetchUserData(string memory _email) external view returns (string[] memory) {
+        return s_userData[_email];
+    }
+
+    function checkDetail(address userAddress) public view returns (bool) {
+        bytes memory userNameBytes = bytes(s_details[userAddress].userName);
+        return (userNameBytes.length > 0);
+    }
+
+    modifier IsNotExisting(address userAddress) {
+        if (bytes(s_details[userAddress].userName).length > 0) {
+            revert DeCert__AlreadyExists();
+        } else {
+            _;
+        }
+    }
+
+    function addUserData(address _userAddress, string memory _data) external {
+        require(s_details[msg.sender].isIssuer, "DeCert__OnlyIssuerRequired");
+        require(_userAddress == msg.sender, "DeCert__Invalid__User");
+        string memory _email = s_details[_userAddress].email;
+        s_userData[_email].push(_data);
+    }
+
+    function updateDetailsByUser(string memory _changeUserName, string memory _changeEmail) external {
+        s_details[msg.sender].userName = _changeUserName;
+        s_details[msg.sender].email = _changeEmail;
+    }
 }
